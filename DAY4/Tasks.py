7fc9d055d0dde4e6895010f1cefe6fd08876998b
Yash <yashpatkar4249@gmail.com>
2026-04-05 14:05:18 +0530

    Refactor Tasks.py: Comment out unused code and clean up formatting

--- DAY4/Tasks.py
+++ DAY4/Tasks.py
@@ -9,7 +9,7 @@
 #Write a function to count the frequency of elemenets in a list using a dictionary
 # a = [1, 2, 3, 3, 5]
 # b = {}
-# for i in a:
+# for i in a,:
 #     if i in b:
 #         b[i] += 1
 #     else:
@@ -19,17 +19,52 @@
 
 
 # count repetitive digits and find repetitive digits and give it as security key and if no repetitive digits are there then print -1
-mylist = [5,7,8,3,7,8,9,2,3]
-newlist = []
+# mylist = [5,7,8,3,7,8,9,2,3]
+# newlist = []
 
-for i in range(len(mylist)):
-    count = 0
-    key= mylist[i]
+# for i in range(len(mylist)):
+#     count = 0
+#     key= mylist[i]
     
-    j = i + 1
-    while j < len(mylist):
-        if key == mylist[j]:
-            count += 1
-            newlist.append(key)
-        j += 1
-print(len(newlist))
+#     j = i + 1
+#     while j < len(mylist):
+#         if key == mylist[j]:
+#             count += 1
+#             newlist.append(key)
+#         j += 1
+# print(len(newlist))
+
+#find the second largest number in a list
+# list = [7,3,9,2,8]
+# list.sort() #sorts the list in ascending order
+# print(list)
+# print(list[-2])
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# username =""  #str
+# password = ""  #str
+# while username != "admin" or password != "admin123":
+#     username = input("Enter username:")
+#     password = input("Enter password:")
+    
+    
+#Count vowels , consonants in a string
+# name = 'programming'
+# vowels=['a', 'e', 'i', 'o', 'u']
+# cons = 0
+# vowel = 0
+# for i in name:
+#     if i in vowels:
+#         vowel +=1
+#     else:
+#         cons +=1
+# print("Vowels: ", vowel)
+# print("Consonants: ", cons)
+
+
+
+    
